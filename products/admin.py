e5c221e9cb9528a6be89ce2ed2e55f64091d0c2e
KamilaAzul <>
2023-09-26 09:37:09 +0000

    Added new model safty

--- products/admin.py
+++ products/admin.py
@@ -1,7 +1,11 @@
 from django.contrib import admin
-from .models import Product, Category, SpeciesCategory
+from .models import Product, Category, SpeciesCategory, Light, Size, CareLevel, Safety
 
 # Register your models here.
 admin.site.register(Product)
 admin.site.register(Category)
 admin.site.register(SpeciesCategory)
+admin.site.register(Light)
+admin.site.register(Size)
+admin.site.register(CareLevel)
+admin.site.register(Safety)
--- products/models.py
+++ products/models.py
@@ -2,7 +2,9 @@ from django.db import models
 
 
 class Category(models.Model):
-
+    """
+    Modal for categories
+    """
     class Meta:
         verbose_name_plural = 'Categories'
 
@@ -35,16 +37,27 @@ class SpeciesCategory(models.Model):
 
 
 class Product(models.Model):
+    """
+    Modal for Product
+    """
     category = models.ForeignKey(
         'Category', null=True, blank=True, on_delete=models.SET_NULL)
     species_category = models.ForeignKey(
         "SpeciesCategory", null=True, blank=True, on_delete=models.SET_NULL
     )
+    care_level = models.ForeignKey(
+        "CareLevel", null=True, blank=True, on_delete=models.SET_NULL
+    )
+    size = models.ForeignKey(
+        "Size", null=True, blank=True, on_delete=models.SET_NULL
+    )
+    light = models.ForeignKey(
+        "Light", null=True, blank=True, on_delete=models.SET_NULL
+    )
     sku = models.CharField(max_length=254, null=True, blank=True)
     name = models.CharField(max_length=254)
     description = models.TextField()
     price = models.DecimalField(max_digits=6, decimal_places=2)
-    has_sizes = models.BooleanField(default=False, null=True, blank=True)
     rating = models.DecimalField(
         max_digits=6, decimal_places=2, null=True, blank=True)
     image_url = models.URLField(max_length=1024, null=True, blank=True)
@@ -52,3 +65,72 @@ class Product(models.Model):
 
     def __str__(self):
         return self.name
+
+
+class CareLevel(models.Model):
+    """
+    Modal for Care level
+    """
+
+    class Meta:
+        verbose_name_plural = "Care levels"
+
+    name = models.CharField(max_length=100)
+    friendly_name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Size(models.Model):
+    """
+    Modal for Plants Sizes
+    """
+    class Meta:
+        verbose_name_plural = "Plants Sizes"
+
+    name = models.CharField(max_length=100)
+    friendly_name = models.CharField(max_length=100, null=True, blank=True)
+   
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Light(models.Model):
+    """
+    Modal for Plants exposure to the light
+    """
+    class Meta:
+        verbose_name_plural = "Light exposure"
+
+    name = models.CharField(max_length=100)
+    friendly_name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
+
+
+class Safety(models.Model):
+    """
+    Modal for safety of children and animals
+    """
+    class Meta:
+        verbose_name_plural = "Safety"
+
+    name = models.CharField(max_length=100)
+    friendly_name = models.CharField(max_length=100, null=True, blank=True)
+
+    def __str__(self):
+        return self.name
+
+    def get_friendly_name(self):
+        return self.friendly_name
