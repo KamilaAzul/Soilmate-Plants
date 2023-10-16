7c66e257eed1c7a586816852049d4c444d3baf6b
KamilaAzul <>
2023-10-16 12:42:12 +0000

    Debug to false, deployment

--- soilmate_plants/settings.py
+++ soilmate_plants/settings.py
@@ -26,7 +26,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['8000-kamilaazul-soilmate-plan-fs2xpzgkxn.us2.codeanyapp.com']
 
