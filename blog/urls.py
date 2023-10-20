a5557a28563f0e3ecf1a824a26c9033feba559f7
KamilaAzul <>
2023-10-20 12:57:36 +0000

    Add update to the templates blog post and detail blog post

--- blog/urls.py
+++ blog/urls.py
@@ -3,7 +3,7 @@ from django.urls import path
 
 
 urlpatterns = [
-    path('', views.PostList.as_view(), name='home'),
+   
     path('blog/', views.AllBlogPost.as_view(), name='all_blog'),
     path('<slug:slug>/', views.PostDetail.as_view(), name='blog_post_detail'),
     
--- blog/views.py
+++ blog/views.py
@@ -8,14 +8,6 @@ from django.contrib.auth.decorators import login_required
 from .forms import PostForm
 from .models import Category, Post
 
-
-class PostList(generic.ListView):
-    model = Post
-    queryset = Post.objects.filter(status=1).order_by("-created_on")
-    template_name = "index.html"
-    paginate_by = 6
-
-
 class AllBlogPost(generic.ListView):
     """
     Render the blog page
@@ -33,6 +25,6 @@ class PostDetail(View):
     def get(self, request, slug):
         queryset = Post.objects.filter(status=1)
         post = get_object_or_404(Post, slug=slug)
-
+    
         return render(request, 'blog/blog_post_detail.html', {'post': post})
 
