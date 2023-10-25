de7d47d6f08ea184889da57d235fbe3c517abff7
KamilaAzul <>
2023-10-25 10:46:37 +0000

    Added newsletter funcionality

--- newsletter/urls.py
+++ newsletter/urls.py
@@ -2,5 +2,5 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('subscribe/', views.SubscribeView.as_view(), name='subscribe'),
+    path('subscribe/', views.subscribe_newsletter, name='subscribe'),
 ]
--- newsletter/views.py
+++ newsletter/views.py
@@ -4,26 +4,37 @@ from .forms import SubscriberForm
 from .models import Subscriber, Newsletter
 from django.core.mail import send_mail
 from django.utils.crypto import get_random_string
-from django.http import HttpResponseRedirect
 from mailchimp3 import MailChimp
+import requests
+from django.contrib import messages
+from requests.exceptions import RequestException
+import os
+import json
+from django.http import JsonResponse
 
+def subscribe_newsletter(request):
+    if request.method == 'POST':
+        email = request.POST.get('email')
+        api_key = os.environ.get('YOUR_MAILCHIMP_API_KEY')
+        list_id = os.environ.get('YOUR_LIST_ID')
+        endpoint = f'https://usX.api.mailchimp.com/3.0/lists/{list_id}/members'
+        data = {
+            'email_address': email,
+            'status': 'pending',
+        }
+        headers = {
+            'Authorization': f'Bearer {api_key}',
+        }
+        try:
+            response = requests.post(endpoint, json=data, headers=headers)
+            response.raise_for_status()  # Raise an exception for HTTP errors
+            if response.status_code == 200:
+                # Subscription successful
+                return JsonResponse({'message': 'Subscription successful'})
+            else:
+                return JsonResponse({'message': 'Failed to subscribe. Please ensure the form is valid.'})
+        except RequestException as e:
+            return JsonResponse({'message': f'Failed to connect to Mailchimp: {str(e)}'})
 
-class SubscribeView(View):
-
-     def post(self, request, *args, **kwargs):
-        email = request.POST.get('email')  
-        api_key = 'YOUR_MAILCHIMP_API_KEY'
-        list_id = 'YOUR_MAILCHIMP_LIST_ID'
-        
-        if email:
-            # Initialize the Mailchimp client
-            client = MailChimp(mc_api=api_key, mc_user='your_username')
-
-            # Subscribe the email to your Mailchimp list
-            client.lists.members.create_or_update(list_id, {'email_address': email}, merge_fields={'FNAME': 'First Name'})
-
-            # You may want to handle success and error responses here
-
-        return HttpResponseRedirect('newsletter/subscription_success.html') 
-           
-
+    # Return a default response (optional)
+    return JsonResponse({'message': 'Invalid request'}, status=400)
