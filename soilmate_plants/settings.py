30b7335807cf8931165527e8e24d446c86f88a62
Kamila Matejczuk <kamila.mate82@gmail.com>
2023-11-10 12:00:00 +0000

    Debug to false

--- soilmate_plants/settings.py
+++ soilmate_plants/settings.py
@@ -28,7 +28,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = os.environ.get('SECRET_KEY', '')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 ALLOWED_HOSTS = ['soilmate-plants-a1626f09724b.herokuapp.com', 'localhost', '8000-kamilaazul-soilmateplan-7clz3updxcr.ws-eu106.gitpod.io']
 
